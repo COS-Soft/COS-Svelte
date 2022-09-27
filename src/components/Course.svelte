c9672f73e329fa797c452d7c273e60734fb7bed8
DoguCigsar <dogucigsar@hotmail.com>
2022-09-27 21:04:03 +0300

    CourseBeta.01

---
+++ src/components/Course.svelte
@@ -0,0 +1,15 @@
+<script> 
+    export let courseID = ''; 
+    import YoutubeVideo from "./YoutubeVideo.svelte";
+</script>
+
+<main>
+    <div class="course">
+        <h1>{courseID}</h1>
+        <YoutubeVideo ytVidId="dQw4w9WgXcQ"/>
+    </div>
+</main>
+
+<style>
+    
+</style>
---
+++ src/routes/courses.svelte
@@ -0,0 +1,34 @@
+<svelte:head>
+    <title>COS Courses</title>
+</svelte:head>
+
+<script>
+    import NavigationBar from "../components/NavigationBar.svelte";
+    import Course from "../components/Course.svelte";
+</script>
+
+<main>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+    <NavigationBar pageName="Courses"/>
+
+    <div class="courses">
+        <h1>Coming Soon!</h1>
+        <Course courseID="COS 126"/>
+    </div>
+</main>
+
+<style>
+    *{
+        font-family: 'Roboto', sans-serif;
+    }
+    .courses{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        margin: 0 auto;
+        max-width: 1200px;
+    }
+</style>
